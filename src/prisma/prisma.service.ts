1c983d100dc77a7a12d2532fd849b1f15158bd3a
YvesJaques <ymoraisjaques@gmail.com>
2023-12-25 20:49:05 -0300

    feat[database] Added prisma Service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+    extends PrismaClient
+    implements OnModuleInit, OnModuleDestroy {
+    public client: PrismaClient
+
+    constructor() {
+        super({
+            log: ['warn', 'error'],
+        })
+    }
+
+    onModuleDestroy() {
+        return this.$connect()
+    }
+
+    onModuleInit() {
+        return this.$disconnect()
+    }
+}
