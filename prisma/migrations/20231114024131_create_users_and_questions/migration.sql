31be5c7aa855880f179895a5aef9ab0ec8e8b93f
YvesJaques <ymoraisjaques@gmail.com>
2023-12-25 23:27:47 -0300

    fix[database] Schema typo

--- prisma/migrations/20231114024131_create_users_and_questions/migration.sql
+++ prisma/migrations/20231114024131_create_users_and_questions/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "passoword" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
