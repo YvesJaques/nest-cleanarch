24254c04e5a30070d91c18ed2aa5f52082e96272
YvesJaques <ymoraisjaques@gmail.com>
2023-12-29 15:56:33 -0300

    test[config] Vitest setup

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipes'
-import { CurrentUser } from 'src/auth/current-user-decorator'
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
-import { UserPayload } from 'src/auth/jwt.strategy'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipes'
+import { CurrentUser } from '@/auth/current-user-decorator'
+import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
+import { UserPayload } from '@/auth/jwt.strategy'
+import { PrismaService } from '@/prisma/prisma.service'
 import { z } from 'zod'
 
 const createQuestionBodySchema = z.object({
