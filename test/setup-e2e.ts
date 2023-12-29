6be3a838d3076a5b99c86a47bdecaaf1d8dea363
YvesJaques <ymoraisjaques@gmail.com>
2023-12-29 16:38:37 -0300

    test[config] Dedicated test db setup

---
+++ test/setup-e2e.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+
+const prisma = new PrismaClient()
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+  const databaseURL = generateUniqueDatabaseURL(randomUUID())
+
+  process.env.DATA_URL = databaseURL
+
+  execSync('yarn prisma migrate deploy')
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`)
+  await prisma.$disconnect()
+})
--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -5,8 +5,10 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
+    exclude: ['**/data/**'],
     globals: true,
     root: './',
+    setupFiles: ['./test/setup-e2e.ts'],
   },
   plugins: [
     tsConfigPaths(),
